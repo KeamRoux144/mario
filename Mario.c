0ccace6f2655bdf02f2d6fe944c2cdf4d1977e30
KeamRoux144 <83517785+KeamRoux144@users.noreply.github.com>
2021-05-01 14:34:00 -0700

    make Mario.c

---
+++ Mario.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+include <stdio.h>
+
+int main (void)
+{
+    do
+    {
+    h = get_int ("height: ");
+    }
+    while (h>1);
+    
+    for (int i=0; i<h; i++)
+    {
+        printf ("#");
+        {
+        printf ("\n")
+}
